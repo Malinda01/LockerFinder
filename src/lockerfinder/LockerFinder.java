661375583a3a3c3ce4a02e77fdfe143053dea301
Malinda <greshanamarakoon@gmail.com>
2025-08-29 18:44:35 +0530

    Update LockerFinder.java

--- src/lockerfinder/LockerFinder.java
+++ src/lockerfinder/LockerFinder.java
@@ -10,12 +10,7 @@ package lockerfinder;
  * 
  */
 public class LockerFinder {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String [] args){
+        
     }
-    
 }
