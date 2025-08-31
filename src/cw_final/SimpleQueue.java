688c73c5cd68268071d0984c83cf3d6651b2fa4d
SathnaraSayuni <sathnarasayuni93@gmail.com>
2025-08-31 15:29:43 +0530

    Update SimpleQue.java

---
+++ src/cw_final/SimpleQueue.java
@@ -0,0 +1,53 @@
+package newpackage;
+
+public class SimpleQueue<T> {
+    private Object[] data;
+    private int head;
+    private int tail;
+    private int size;
+
+    public SimpleQueue() { this(8); }
+    public SimpleQueue(int initialCapacity) {
+        data = new Object[Math.max(2, initialCapacity)];
+        head = tail = size = 0;
+    }
+
+    public boolean isEmpty() { return size == 0; }
+    public int size() { return size; }
+
+    public void enqueue(T value) {
+        ensureCapacity(size + 1);
+        data[tail] = value;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T dequeue() {
+        if (isEmpty()) return null;
+        T val = (T) data[head];
+        data[head] = null;
+        head = (head + 1) % data.length;
+        size--;
+        return val;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T peek() {
+        if (isEmpty()) return null;
+        return (T) data[head];
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity <= data.length) return;
+        int newCap = data.length * 2;
+        while (newCap < minCapacity) newCap *= 2;
+        Object[] newData = new Object[newCap];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(head + i) % data.length];
+        }
+        data = newData;
+        head = 0;
+        tail = size;
+    }
+}
