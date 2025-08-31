7b737bf1b2a8f2ba705b0a8e42e7e0c271a6fcb0
SathnaraSayuni <sathnarasayuni93@gmail.com>
2025-08-31 15:07:36 +0530

    implement PathResult.java

---
+++ src/cw_final/PathResult.java
@@ -0,0 +1,7 @@
+package newpackage;
+
+public class PathResult {
+    public final int[] dist;
+    public final int[] prev;
+    public PathResult(int[] dist, int[] prev) { this.dist = dist; this.prev = prev; }
+}
